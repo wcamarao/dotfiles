bfc16a1dca54de537dd7280c22c753185aac48ee
Wagner Camarao <functioncallback@gmail.com>
2013-06-07 21:01:35 -0700

    html5

--- model/alias.zsh
+++ model/alias.zsh
@@ -4,3 +4,4 @@ alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
 alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
 alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+alias be='bundle exec '
