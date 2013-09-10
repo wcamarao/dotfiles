64607636e7d04459c8f6050331e8fcd00483f568
Wagner Camarao <wagner@camarao.io>
2013-09-10 15:47:24 -0700

    Remove lame sleep statement

--- model/functions.zsh
+++ model/functions.zsh
@@ -40,6 +40,6 @@ function fs() {
 # Serve a directory via http, optionally specifying the port
 function server() {
   local port="${1:-8000}"
-  sleep 1 && open "http://localhost:${port}/" &
+  open "http://localhost:${port}/" &
   python -m SimpleHTTPServer ${port}
 }
